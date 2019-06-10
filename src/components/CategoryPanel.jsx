399a9749c53234292625c8d7c5599c6ec94f8f15
sebdybowski <dybowski@int.pl>
2019-06-11 00:18:52 +0200

    bugs fixed

--- src/components/CategoryPanel.jsx
+++ src/components/CategoryPanel.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const CategoryPanel = () => <ul className="nav flex-column">
-    <li className="nav-item">
-        <a className="nav-link active" href="#">Sports</a>
+    <h5 className="mb-3">Data sources</h5>
+    <li className="nav-item mb-2">
+        <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="sports" />
+            <label className="form-check-label" htmlFor="sports">
+                <span className="btn-link">Sports</span>
+            </label>
+        </div>
     </li>
-    <li className="nav-item">
-        <a className="nav-link" href="#">Fashion</a>
+    <li className="nav-item mb-2">
+        <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="fashion" />
+            <label className="form-check-label" htmlFor="fashion">
+                <span className="btn-link">Fashion</span>
+            </label>
+        </div>
     </li>
 </ul>;
