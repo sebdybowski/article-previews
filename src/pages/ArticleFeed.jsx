7cad5d10350f7b706e3f3a227a79257114180fc8
sebdybowski <dybowski@int.pl>
2019-06-10 22:07:50 +0200

    babel configured

---
+++ src/pages/ArticleFeed.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export class ArticleFeed extends Component {
+    componentDidMount() {
+
+    }
+    render () {
+        return <div>Hello World</div>;
+    }
+}
