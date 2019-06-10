32d447dd6e1e506694b3db7d5567a3faac97be36
sebdybowski <dybowski@int.pl>
2019-06-10 22:56:58 +0200

    fetching basics resolved

---
+++ src/utils/services.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import get from 'lodash.get';
+import flatten from 'lodash.flatten';
+import { ARTICLE_TYPE } from './constants';
+
+const { SPORTS, FASHION } = ARTICLE_TYPE;
+
+export const fetchArticle = type => axios.get(`http://localhost:6010/articles/${type}`)
+    .then(response => get(response, 'data.articles'))
+    .catch(error => {
+        console.log(error);
+        return error;
+    });
+
+export const fetchAllArticles = () => Promise.all([fetchArticle(SPORTS), fetchArticle(FASHION)])
+    .then(response => flatten(response))
+    .catch(error => console.log(error));
