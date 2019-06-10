06f2a54bc41730cbaaec0720da1956d2cc95ff4d
sebdybowski <dybowski@int.pl>
2019-06-10 23:51:22 +0200

    articles are rendering + some boilerplates on navigation created

--- src/pages/ArticleFeed.jsx
+++ src/pages/ArticleFeed.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import map from 'lodash.map';
+import get from 'lodash.get';
 import { fetchAllArticles } from '../utils/services';
+import { ArticlePreview } from '../components/ArticlePreview';
+import { CategoryPanel } from "../components/CategoryPanel";
 
 const INITIAL_STATE = {
   articles: [],
 };
+
+const Articles = ({ articles }) => map(articles, article =>
+    <ArticlePreview article={article} key={article.id}/>);
 
 export class ArticleFeed extends Component {
     constructor(props) {
@@ -15,6 +22,18 @@ export class ArticleFeed extends Component {
     }
     render () {
         console.log(this.state);
-        return <div>Hello World</div>;
+        return <div className="container">
+            <nav className="row">
+                <div className="col">filter</div>
+            </nav>
+            <main className="row">
+                <nav className="col-3" >
+                    <CategoryPanel />
+                </nav>
+                <section className="col-9">
+                    <Articles articles={get(this, 'state.articles')} />
+                </section>
+            </main>
+        </div>;
     }
 }
